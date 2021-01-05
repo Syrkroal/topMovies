26644b803aed62cf7b3c6ac006b1ae0b41ea1209
Syrkroal <35395892+Syrkroal@users.noreply.github.com>
2021-01-05 14:58:14 +0100

    movie api with TMDb apis

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const moviesRoute = require('./routes/movies');
+
+app.use('/movies', moviesRoute);
+
+app.listen(4000, () => {
+    console.log("server on localhost");
+})
