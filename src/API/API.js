e5dc084b047485cd03141790a8cf030d9bf696c5
Syrkroal <35395892+Syrkroal@users.noreply.github.com>
2021-01-06 22:39:13 +0100

    api url path

--- src/API/API.js
+++ src/API/API.js
@@ -1,4 +1,4 @@
 export let url =
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
         ? " http://localhost:4000"
-        : "https://topmovies-backend.herokuapp.com/";
+        : "https://topmovies-backend.herokuapp.com";
